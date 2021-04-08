10ee757d8ffc21dbc79b1bc33a59689acc943828
SHaider0 <Samnoon.Haider@gmail.com>
2021-04-08 17:17:43 -0400

    remove title so property form component returns only the form

--- src/PropertyForm.js
+++ src/PropertyForm.js
@@ -2,18 +2,15 @@ import React from "react"
 
 const PropertyForm = () => {
     return (
-        <>
-            <h1>Enter Property Details</h1>
-            <form>
-                <div>
-                    <label for="purchase-price">Purchase Price</label>
-                    <input type="text" id="purchase-price" />
-                </div>
-                <div>
-                    <button type="submit">Calculate</button>
-                </div>
-            </form>
-        </>
+        <form id="property-form">
+            <div>
+                <label for="purchase-price">Purchase Price</label>
+                <input type="text" id="purchase-price" />
+            </div>
+            <div>
+                <button type="submit">Calculate</button>
+            </div>
+        </form>
     )
 }
 
