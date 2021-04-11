a67addfa2c8713b56f54777891692e7ee962bdd5
SHaider0 <Samnoon.Haider@gmail.com>
2021-04-11 11:28:46 -0400

    change type to number on purchase price input

--- src/PropertyForm.js
+++ src/PropertyForm.js
@@ -38,10 +38,16 @@ class PropertyForm extends React.Component {
     render() {
         return (
             <form id="property-form" onSubmit={this.handleSubmit}>
+                {/* <div>
+                    <label>
+                        Property Address
+                        <input type="text" id="property-address" value={this.state.propertyAddress} onChange={this.handleChange} />
+                    </label>
+                </div> */}
                 <div>
                     <label>
                         Purchase Price
-                        <input type="text" id="purchase-price" value={this.state.purchasePrice} onChange={this.handleChange} />
+                        <input type="number" id="purchase-price" value={this.state.purchasePrice} onChange={this.handleChange} />
                     </label>
                 </div>
                 <div>
